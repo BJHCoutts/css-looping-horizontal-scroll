6cda49169767cacda8d7f76704539906453488aa
Brian Coutts <bjhcoutts@gmail.com>
2023-10-23 13:49:43 -0700

    basic setup and styles

---
+++ src/lib/components/HorizontalScroll.svelte
@@ -0,0 +1,30 @@
+<ul class="media-scroller">
+	<li class='media-element'></li>
+	<li class='media-element'></li>
+	<li class='media-element'></li>
+	<li class='media-element'></li>
+	<li class='media-element'></li>
+	<li class='media-element'></li>
+	<li class='media-element'></li>
+	<li class='media-element'></li>
+	<li class='media-element'></li>
+</ul>
+
+<style>
+
+	.media-scroller {
+		display: grid;
+		grid-auto-flow: column;
+		grid-auto-columns: 15%;
+		grid-column-gap: 50px;
+		overflow-x: auto;
+		overscroll-behavior-inline: contain;
+	}
+
+	.media-element {
+		background-color: rgb(40, 40, 112);
+		height: 200px;
+		width: 100%;
+	}
+
+</style>
