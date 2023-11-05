4d5bcffcda15509912bbc0b80c71b2e2ba99fc75
Brian Coutts <bjhcoutts@gmail.com>
2023-11-04 19:01:24 -0700

    dark mode styles

--- src/lib/components/HorizontalScroll.svelte
+++ src/lib/components/HorizontalScroll.svelte
@@ -4,53 +4,86 @@
 
 	onMount( () => {
 
-		const scroller = document.querySelector('.media-scroller')
+		const scrollers = document.querySelectorAll(".scroller-container");
 
+		// If a user hasn't opted in for recuded motion, then we add the animation
 		if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-			addAnimation()
+			addAnimation();
 		}
 
 		function addAnimation() {
-			if (!scroller?.children) return
-			scroller.setAttribute('data-animated', 'true')
-			const scrollerElementsAsArray = Array.from(scroller.children)
-			scrollerElementsAsArray.forEach( (el) => {
-				const duplicateEl = el.cloneNode(true)
-				duplicateEl.setAttribute('aria-hidden', true)
-				scroller.appendChild(duplicateEl)
-			})
+			scrollers.forEach((scroller) => {
+				// add data-animated="true" to every `.scroller` on the page
+				scroller.setAttribute("data-animated", true);
+
+				// Make an array from the elements within `.scroller-inner`
+				const scrollerInner = scroller.querySelector(".scroller__inner");
+				const scrollerContent = Array.from(scrollerInner.children);
+
+				// For each item in the array, clone it
+				// add aria-hidden to it
+				// add it into the `.scroller-inner`
+				scrollerContent.forEach((item) => {
+					const duplicatedItem = item.cloneNode(true);
+					duplicatedItem.setAttribute("aria-hidden", true);
+					scrollerInner.appendChild(duplicatedItem);
+				});
+			});
 		}
 	})
 
 </script>
 
-<h1 style="text-align: center">Infinite Scroll Animation</h1>
+<section>
 
-<div class="scroller-container" data-speed="fast">
-  <ul class="tag-list scroller__inner">
-    <li>HTML</li>
-    <li>CSS</li>
-    <li>JS</li>
-    <li>SSG</li>
-    <li>webdev</li>
-    <li>animation</li>
-    <li>UI/UX</li>
-  </ul>
-</div>
+	<h1 style="text-align: center">Infinite Scroll Animation</h1>
+	
+	<div class="scroller-container" data-speed="fast">
+		<ul class="tag-list scroller__inner">
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>JS</li>
+			<li>SSG</li>
+			<li>webdev</li>
+			<li>animation</li>
+			<li>UI/UX</li>
+		</ul>
+	</div>
+	
+	<div class="scroller-container" data-direction="right" data-speed="slow">
+		<div class="scroller__inner">
+			<img src="https://i.pravatar.cc/150?img=1" alt="" />
+			<img src="https://i.pravatar.cc/150?img=2" alt="" />
+			<img src="https://i.pravatar.cc/150?img=3" alt="" />
+			<img src="https://i.pravatar.cc/150?img=4" alt="" />
+			<img src="https://i.pravatar.cc/150?img=5" alt="" />
+			<img src="https://i.pravatar.cc/150?img=6" alt="" />
+		</div>
+	</div>
 
-<div class="scroller-container" data-direction="right" data-speed="slow">
-  <div class="scroller__inner">
-    <img src="https://i.pravatar.cc/150?img=1" alt="" />
-    <img src="https://i.pravatar.cc/150?img=2" alt="" />
-    <img src="https://i.pravatar.cc/150?img=3" alt="" />
-    <img src="https://i.pravatar.cc/150?img=4" alt="" />
-    <img src="https://i.pravatar.cc/150?img=5" alt="" />
-    <img src="https://i.pravatar.cc/150?img=6" alt="" />
-  </div>
-</div>
+</section>
+
 
 <style>
 
+:root {
+  --clr-neutral-100: hsl(0, 0%, 100%);
+  --clr-primary-100: hsl(205, 15%, 58%);
+  --clr-primary-400: hsl(215, 25%, 27%);
+  --clr-primary-800: hsl(217, 33%, 17%);
+  --clr-primary-900: hsl(218, 33%, 9%);
+}
+
+	section {
+		color-scheme: dark;
+		display: grid;
+		min-block-size: 100vh;
+		place-content: center;
+		font-family: system-ui;
+		font-size: 1.125rem;
+		background-color: var(--clr-primary-800);
+		color: var(--clr-neutral-100);
+	}
 	h1 {
 		text-wrap: balance;
 		text-align: center;
@@ -128,8 +161,17 @@
 		scroll-snap-align: start;
 	}
 
-	ul {
+	.tag-list {
 		list-style: none;
+		padding-inline: 0;
+		margin: 0;
+	}
+
+	.tag-list > li {
+		padding: 1rem;
+		background: var(--clr-primary-400);
+		border-radius: 0.5rem;
+		box-shadow: 0 0.5rem 1rem -0.25rem var(--clr-primary-900);
 	}
 
 </style>
